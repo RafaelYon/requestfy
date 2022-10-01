045f8461b808bf1a07ac2e20043f81e32322d9bd
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2022-10-01 17:45:33 -0300

    docs(client): added basic documentation

--- client.go
+++ client.go
@@ -2,14 +2,17 @@ package requestfy
 
 import "net/http"
 
+// RequestExecuter abstracts the http client used behind the scenes to perform HTTP requests
 type RequestExecuter interface {
 	Do(*http.Request) (*http.Response, error)
 }
 
+// Client Allows you to perform http requests with a simple syntax
 type Client struct {
 	executer RequestExecuter
 }
 
+// ClientConfig is a function to configure the client
 type ClientConfig func(*Client)
 
 func NewClient(configs ...ClientConfig) *Client {
@@ -22,6 +25,7 @@ func NewClient(configs ...ClientConfig) *Client {
 	return client
 }
 
+// ConfigRequestExecuter allows you to specify an http request executor for the client
 func ConfigRequestExecuter(executer RequestExecuter) ClientConfig {
 	return func(c *Client) {
 		c.executer = executer
