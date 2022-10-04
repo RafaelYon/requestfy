00b2cbf8050dd347a79d08a38cf0a097d11d0c76
Rafael Antonio Yon <rafaelyon.1999@gmail.com>
2022-10-03 23:41:42 -0300

    test(response): added validation of decoded values

--- response_test.go
+++ response_test.go
@@ -34,6 +34,20 @@ func TestResponseJsonDecode(t *testing.T) {
 	if total := len(endpoints); total != 6 {
 		t.Errorf("expected 6 json decoded endpoints, received '%d'", total)
 	}
+
+	expectedEndpoints := map[string]string{
+		"people":    "https://swapi.dev/api/people/",
+		"planets":   "https://swapi.dev/api/planets/",
+		"films":     "https://swapi.dev/api/films/",
+		"species":   "https://swapi.dev/api/species/",
+		"vehicles":  "https://swapi.dev/api/vehicles/",
+		"starships": "https://swapi.dev/api/starships/",
+	}
+	for expectedKey, expectedVal := range expectedEndpoints {
+		if val := endpoints[expectedKey]; val != expectedVal {
+			t.Errorf("expected '%s' endpoint in the key '%s', received '%s'", expectedVal, expectedKey, val)
+		}
+	}
 }
 
 type requestExecuterStub struct {
