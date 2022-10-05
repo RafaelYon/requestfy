57fe1f530d3c5bca40d8c3d3c981fa361321aa4a
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-10-05 22:39:35 +0700

    fix(request): send body on post request

--- request.go
+++ request.go
@@ -31,7 +31,7 @@ func (r *Request) Get(url string) (*Response, error) {
 
 // Post performs a request using the POST method
 func (r *Request) Post(url string, body io.Reader) (*Response, error) {
-	req, err := r.client.newRequest(r.context, url, http.MethodPost, nil)
+	req, err := r.client.newRequest(r.context, url, http.MethodPost, body)
 	if err != nil {
 		return nil, err
 	}
